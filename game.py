1fa00c0d8fdb0fd7a0871eeca2c0de3bc1daef37
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2022-10-30 19:59:00 +0900

    🎉 minimax playable

--- game.py
+++ game.py
@@ -1,9 +1,14 @@
+import random
+from copy import deepcopy
 from typing import List
 
-# def iter_3x3():
-#     for x in range(3):
-#         for y in range(3):
-#             yield (x, y)
+DEPTH = 2
+
+
+def iter_3x3():
+    for x in range(3):
+        for y in range(3):
+            yield (x, y)
 
 
 def tic_tac_toe_winner(board: List[List[int]]):
@@ -45,16 +50,18 @@ class UltimateTicTacToe:
     def __str__(self):
         return str_board(self.board)
 
-    def nth_sub_board(self, n: int):
-        sx, sy = n % 3, n // 3
-        return self.sub_board(sx, sy)
-
     def sub_board(self, sx: int, sy: int):
         sx, sy = sx * 3, sy * 3
         sub = []
         for x in range(sx, sx + 3):
             sub.append([self.board[x][y] for y in range(sy, sy + 3)])
         return sub
+
+    def meta_board(self):
+        meta = []
+        for x in range(3):
+            meta.append([tic_tac_toe_winner(self.sub_board(x, y)) for y in range(3)])
+        return meta
 
     def flip_turn(self):
         self.turn = 2 if self.turn == 1 else 1
@@ -63,9 +70,10 @@ class UltimateTicTacToe:
         if self.board[x][y] != 0:
             # Illegal act
             self.done = True
-            return
+            return self
         self.board[x][y] = player
         self.flip_turn()
+        return self
 
     def legal_actions(self):
         legal = []
@@ -79,19 +87,90 @@ class UltimateTicTacToe:
         while not self.done:
             print(self)
             # Replace with AI here.
-            x, y = map(int, input("Move>").split())
+            if self.turn == 1:
+                x, y = map(int, input("Move>").split())
+            else:
+                legal_actions = self.legal_actions()
+                action_rewards = [
+                    minimax(
+                        deepcopy(self).act(self.turn, x, y),
+                        DEPTH,
+                        self.turn,
+                    )
+                    for x, y in legal_actions
+                ]
+                # print(legal_actions)
+                # print(action_rewards)
+                max_reward = max(action_rewards)
+                max_actions = [
+                    a for a, r in zip(legal_actions, action_rewards) if r == max_reward
+                ]
+                x, y = random.choice(max_actions)
+
             self.act(self.turn, x, y)
-            for i in range(9):
-                print(str_board(self.nth_sub_board(i)))
+            for x, y in iter_3x3():
+                print(str_board(self.sub_board(x, y)))
             print("meta board")
 
-            meta = []
-            for x in range(3):
-                meta.append(
-                    [tic_tac_toe_winner(self.sub_board(x, y)) for y in range(3)]
-                )
-            print(str_board(meta))
-            print(tic_tac_toe_winner(meta))
+            print(str_board(self.meta_board()))
+            print(tic_tac_toe_winner(self.meta_board()))
+
+
+def score(game: UltimateTicTacToe, player: int):
+    assert player in [1, 2]
+    meta_win = tic_tac_toe_winner(game.meta_board())
+    if meta_win != 0:
+        return 10 if meta_win == player else -10
+
+    sub_wins = [tic_tac_toe_winner(game.sub_board(x, y)) for x, y in iter_3x3()]
+    p1 = sub_wins.count(1)
+    p2 = sub_wins.count(2)
+    return p1 - p2 if player == 1 else p2 - p1
+
+
+def minimax(game: UltimateTicTacToe, depth: int, maximizing_player: int) -> float:
+    """
+    MiniMax function
+    """
+    # Ref: https://en.wikipedia.org/wiki/Minimax
+    return alphabeta(game, depth, maximizing_player, -float("inf"), float("inf"))
+
+
+def alphabeta(
+    game: UltimateTicTacToe,
+    depth: int,
+    maximizing_player: int,
+    alpha: float,
+    beta: float,
+) -> float:
+    """
+    MiniMax with alpha-beta pruning
+    """
+    # Ref: https://en.wikipedia.org/wiki/Alpha–beta_pruning
+    if depth == 0 or game.done or len(game.legal_actions()) == 0:
+        return score(game, maximizing_player)
+
+    legal_actions = game.legal_actions()
+    if game.turn == maximizing_player:
+        for x, y in legal_actions:
+            child = deepcopy(game)
+            child.act(game.turn, x, y)
+            alpha = max(
+                alpha, alphabeta(child, depth - 1, maximizing_player, alpha, beta)
+            )
+            if alpha >= beta:
+                break
+        return alpha
+    else:
+        for x, y in legal_actions:
+            child = deepcopy(game)
+            child.act(game.turn, x, y)
+            beta = min(
+                beta, alphabeta(child, depth - 1, maximizing_player, alpha, beta)
+            )
+            if alpha >= beta:
+                break
+        return beta
 
 
 if __name__ == "__main__":
